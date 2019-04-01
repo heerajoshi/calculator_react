62279bd6b0346416a235377244e9db54af6568bc
Ballabh Heera <44017701+heerajoshi@users.noreply.github.com>
2019-04-01 15:30:00 +0530

    Create a sample calculator in react js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  handlekey(e) {
+    let inputDiv = document.getElementById("input");
+    inputDiv.innerText += e.target.innerText;
+  }
+
+  evaluate(){
+    let inputDiv = document.getElementById("input");
+    inputDiv.innerText = eval(inputDiv.innerText);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="inputBox">
+          <div id="input" class="inputText" />
+        </div>
+        <div className="row">
+          <button className="btn" onClick={this.handlekey}>
+            7
+          </button>
+          <button className="btn" onClick={this.handlekey}>
+            8
+          </button>
+          <button className="btn" onClick={this.handlekey}>
+            9
+          </button>
+          <button className="btn" onClick={this.handlekey}>
+            +
+          </button>
+        </div>
+
+        <div className="row">
+          <button className="btn" onClick={this.handlekey}>
+            4
+          </button>
+          <button className="btn" onClick={this.handlekey}>
+            5
+          </button>
+          <button className="btn" onClick={this.handlekey}>
+            6
+          </button>
+          <button className="btn" onClick={this.handlekey}>
+            *
+          </button>
+        </div>
+
+        <div className="row">
+          <button className="btn" onClick={this.handlekey}>
+            1
+          </button>
+          <button className="btn" onClick={this.handlekey}>
+            2
+          </button>
+          <button className="btn" onClick={this.handlekey}>
+            3
+          </button>
+          <button className="btn" onClick={this.handlekey}>
+            /
+          </button>
+        </div>
+
+        <div className="row">
+          <button className="btn" onClick={this.handlekey}>
+            -
+          </button>
+          <button className="btn" onClick={this.handlekey}>
+            0
+          </button>
+          <button className="btn" onClick={this.handlekey}>
+            .
+          </button>
+          <button className="btn" onClick={this.evaluate}>
+            =
+          </button>
+        </div>
       </div>
     );
   }
